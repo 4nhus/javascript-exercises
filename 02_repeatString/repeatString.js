be4f7e0aab002f732f061477ca6f4c4c0f5d48cb
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-09-10 16:37:50 +1000

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, numRepeats) {
+    if (numRepeats < 0) {
+        return "ERROR";
+    } else if (numRepeats === 0) {
+        return "";
+    }
 
+    let repeatString = string;
+
+    for (let i = 1; i < numRepeats; i++) {
+        repeatString += string;
+    }
+
+    return repeatString;
 };
 
 // Do not edit below this line
