c0d897e2be611f08b7c73434ff853e29c470e1f6
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-09-10 18:44:34 +1000

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    for (let person of array) {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    }
 
+    array.sort((personLeft, personRight) => (personLeft.yearOfDeath - personLeft.yearOfBirth) - (personRight.yearOfDeath - personRight.yearOfBirth));
+
+    return array[array.length - 1];
 };
 
 // Do not edit below this line
