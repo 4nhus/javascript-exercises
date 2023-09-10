51aac44a9c4a990f61930ab23950be32655bba41
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-09-10 18:14:35 +1000

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    string = string.toLowerCase().replaceAll(/[^a-z0-9]/g, "");
 
+    for (let i = 0, j = string.length - 1; i < j; i++, j--) {
+        if (string[i] != string[j]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
