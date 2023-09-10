0400d2ca2dcba9d5148c059763896bb45b416d5e
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-09-10 17:14:55 +1000

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    for (let i = 1; i < arguments.length; i++) {
+        const indexOfElementToDelete = array.indexOf(arguments[i]);
 
+        if (indexOfElementToDelete === -1) continue;
+
+        const leftHalf = array.slice(0, indexOfElementToDelete);
+        const rightHalf = array.slice(indexOfElementToDelete + 1);
+        array = leftHalf.concat(rightHalf);
+    }
+
+    return array;
 };
 
 // Do not edit below this line
